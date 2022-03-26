2bb9a8008661bd67035aa9bf62ebc5803c9bfaf2
Excel Olatunbosun <xclolatunbosun@gmail.com>
2022-03-26 22:54:38 +0100

    connecting to mongodb

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-import express from 'express'
+import express from "express";
+import mongoose from 'mongoose'
 
-const app = express()
+const app = express();
+
+const mongoUri = 'mongodb+srv://theexcel:olatunbosun2018@cluster0.e2qzr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongoose.connect(mongoUri, () => {
+    console.log('connected to mongodb')
+})
+
+
+app.get("/", (req, res) => {
+  res.send("Hi there!");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
